c38a330594606f55e0ce15c2e7eb0109c0ae3e09
Jeffrey Liu <jeffkliu@gmail.com>
2024-06-07 19:21:28 -0700

    Reapply "updated navbar to allow click on search to drop down results"
    This reverts commit f0efab6ff4ff1808ea0a0838a27c3979cabf0df1.

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { fetchStockSuggestions } from '../services/polygonService';
 import './Navbar.css';
@@ -6,14 +6,32 @@ import './Navbar.css';
 const Navbar = ({ fetchStockData }) => {
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [lastSuggestions, setLastSuggestions] = useState([]);
+  const searchRef = useRef(null);
 
   useEffect(() => {
     if (query.length > 1) {
-      fetchStockSuggestions(query).then(setSuggestions);
+      fetchStockSuggestions(query).then((results) => {
+        setSuggestions(results);
+        setLastSuggestions(results);
+      });
     } else {
       setSuggestions([]);
     }
   }, [query]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setSuggestions([]);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [searchRef]);
 
   const handleSearch = (event) => {
     event.preventDefault();
@@ -25,6 +43,12 @@ const Navbar = ({ fetchStockData }) => {
     setQuery(suggestion.ticker);
     fetchStockData(suggestion.ticker);
     setSuggestions([]);
+  };
+
+  const handleFocus = () => {
+    if (lastSuggestions.length > 0) {
+      setSuggestions(lastSuggestions);
+    }
   };
 
   return (
@@ -46,7 +70,8 @@ const Navbar = ({ fetchStockData }) => {
         <div className='collapse navbar-collapse' id='navbarNav'>
           <form
             className='d-flex ms-auto search-container'
-            onSubmit={handleSearch}>
+            onSubmit={handleSearch}
+            ref={searchRef}>
             <div className='search-wrapper'>
               <input
                 className='form-control'
@@ -55,6 +80,7 @@ const Navbar = ({ fetchStockData }) => {
                 aria-label='Search'
                 value={query}
                 onChange={(e) => setQuery(e.target.value.toUpperCase())}
+                onFocus={handleFocus}
               />
               {suggestions.length > 0 && (
                 <ul className='list-group'>
