ba99a38897830858387b8361fb62eb52694864ac
Jeffrey Liu <jeffkliu@gmail.com>
2024-06-13 19:49:11 -0700

    refactored redux files

--- src/redux/actions/newsActions.js
+++ src/redux/actions/newsActions.js
@@ -1,14 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const apiKey = process.env.REACT_APP_POLYGON_API_KEY;
 
-export const fetchNews = createAsyncThunk('news/fetchNews', async (symbol) => {
-  const response = await fetch(
-    `https://api.polygon.io/v2/reference/news?ticker=${symbol}&limit=12&apiKey=${apiKey}`
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch news');
+export const fetchNews = createAsyncThunk(
+  'news/fetchNews',
+  async (symbol, { rejectWithValue }) => {
+    console.log(`Fetching news for symbol: ${symbol}`);
+    try {
+      const response = await axios.get(
+        `https://api.polygon.io/v2/reference/news?ticker=${symbol}&apiKey=${apiKey}`
+      );
+      console.log('News data:', response.data);
+      return response.data.results;
+    } catch (error) {
+      console.error('Error fetching news:', error);
+      if (error.response && error.response.status === 403) {
+        return rejectWithValue(
+          'Access forbidden: check API key and permissions.'
+        );
+      } else {
+        return rejectWithValue('Error fetching news');
+      }
+    }
   }
-  const data = await response.json();
-  return data.results;
-});
+);
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,19 +1,22 @@
-import { SET_USER_SYMBOL, SET_STOCK_PRICE } from '../actions/userActions';
+import { createSlice } from '@reduxjs/toolkit';
+import { setUserSymbol, setStockPrice } from '../actions/userActions';
 
-const initialState = {
-  symbol: null,
-  stockPrice: null,
-};
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    symbol: '',
+    stockPrice: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(setUserSymbol, (state, action) => {
+        state.symbol = action.payload;
+      })
+      .addCase(setStockPrice, (state, action) => {
+        state.stockPrice = action.payload;
+      });
+  },
+});
 
-const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_USER_SYMBOL:
-      return { ...state, symbol: action.payload };
-    case SET_STOCK_PRICE:
-      return { ...state, stockPrice: action.payload };
-    default:
-      return state;
-  }
-};
-
-export default userReducer;
+export default userSlice.reducer;
