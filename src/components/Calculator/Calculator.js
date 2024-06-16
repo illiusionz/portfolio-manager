c773fd9efad05115bd8610831c5124536b42fa14
Jeffrey Liu <jeffkliu@gmail.com>
2024-06-15 18:50:03 -0700

    added colors to calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -33,19 +33,31 @@ const Calculator = () => {
         <button onClick={() => handleClick('1')}>1</button>
         <button onClick={() => handleClick('2')}>2</button>
         <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('+')}>+</button>
+        <button className='calculator-action' onClick={() => handleClick('+')}>
+          +
+        </button>
         <button onClick={() => handleClick('4')}>4</button>
         <button onClick={() => handleClick('5')}>5</button>
         <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('-')}>-</button>
+        <button className='calculator-action' onClick={() => handleClick('-')}>
+          -
+        </button>
         <button onClick={() => handleClick('7')}>7</button>
         <button onClick={() => handleClick('8')}>8</button>
         <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('*')}>*</button>
+        <button className='calculator-action' onClick={() => handleClick('*')}>
+          *
+        </button>
         <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={handleClear}>C</button>
-        <button onClick={handleCalculate}>=</button>
-        <button onClick={() => handleClick('/')}>/</button>
+        <button className='calculator-action' onClick={handleClear}>
+          C
+        </button>
+        <button className='calculator-action' onClick={handleCalculate}>
+          =
+        </button>
+        <button className='calculator-action' onClick={() => handleClick('/')}>
+          /
+        </button>
       </div>
     </div>
   );
