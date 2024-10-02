3b524bfc38fb66ff70e38a6fb05b747a3c8c3eca
Jeffrey Liu <jeffkliu@gmail.com>
2024-10-01 19:06:29 -0700

    Revert "added storybook"
    This reverts commit d975b97b78cb37dd580b6398046923737e153512.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppProviders from './app/AppProviders';
-//import { Provider } from 'react-redux';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './app/App';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
